392b92d1798d5797b08fa0954d00e6b6b5f490d5
Saad-khan-18 <saadmaniyarsaadmaniyar@gmail.com>
2024-06-24 12:34:27 +0530

    this is my second commit

---
+++ Application.java
@@ -0,0 +1,49 @@
+//Application -> -> declare 9 static variables and 8 local variables and display it?
+
+public class Application {
+    // Declaring 9 static variables
+    static int staticVar1 = 1;
+    static int staticVar2 = 2;
+    static int staticVar3 = 3;
+    static int staticVar4 = 4;
+    static int staticVar5 = 5;
+    static int staticVar6 = 6;
+    static int staticVar7 = 7;
+    static int staticVar8 = 8;
+    static int staticVar9 = 9;
+
+    public static void main(String[] args) {
+        // Declaring 8 local variables
+        int localVar1 = 10;
+        int localVar2 = 20;
+        int localVar3 = 30;
+        int localVar4 = 40;
+        int localVar5 = 50;
+        int localVar6 = 60;
+        int localVar7 = 70;
+        int localVar8 = 80;
+
+        // Displaying static variables
+        System.out.println("Static Variables:");
+        System.out.println("staticVar1: " + staticVar1);
+        System.out.println("staticVar2: " + staticVar2);
+        System.out.println("staticVar3: " + staticVar3);
+        System.out.println("staticVar4: " + staticVar4);
+        System.out.println("staticVar5: " + staticVar5);
+        System.out.println("staticVar6: " + staticVar6);
+        System.out.println("staticVar7: " + staticVar7);
+        System.out.println("staticVar8: " + staticVar8);
+        System.out.println("staticVar9: " + staticVar9);
+
+        // Displaying local variables
+        System.out.println("Local Variables:");
+        System.out.println("localVar1: " + localVar1);
+        System.out.println("localVar2: " + localVar2);
+        System.out.println("localVar3: " + localVar3);
+        System.out.println("localVar4: " + localVar4);
+        System.out.println("localVar5: " + localVar5);
+        System.out.println("localVar6: " + localVar6);
+        System.out.println("localVar7: " + localVar7);
+        System.out.println("localVar8: " + localVar8);
+    }
+}
