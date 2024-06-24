18659c0a4eb9992ec37cd79443f3e33c12329fcc
Saad-khan-18 <saadmaniyarsaadmaniyar@gmail.com>
2024-06-24 13:01:47 +0530

    this is fourth commit

---
+++ Watch.java
@@ -0,0 +1,38 @@
+//Watch ->declare 12 static variables and display it
+
+public class Watch {
+    // Declare 12 static variables
+    static int var1 = 1;
+    static int var2 = 2;
+    static int var3 = 3;
+    static int var4 = 4;
+    static int var5 = 5;
+    static int var6 = 6;
+    static int var7 = 7;
+    static int var8 = 8;
+    static int var9 = 9;
+    static int var10 = 10;
+    static int var11 = 11;
+    static int var12 = 12;
+
+    // Method to display the variables
+    public static void displayVariables() {
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+        System.out.println("var5: " + var5);
+        System.out.println("var6: " + var6);
+        System.out.println("var7: " + var7);
+        System.out.println("var8: " + var8);
+        System.out.println("var9: " + var9);
+        System.out.println("var10: " + var10);
+        System.out.println("var11: " + var11);
+        System.out.println("var12: " + var12);
+    }
+
+    public static void main(String[] args) {
+        // Display the static variables
+        displayVariables();
+    }
+}
