47a18dcf0c61808f3e52cbe85d09a03c19186f15
Saad-khan-18 <saadmaniyarsaadmaniyar@gmail.com>
2024-06-24 12:43:20 +0530

    this is third commit

---
+++ Checksum.java
@@ -0,0 +1,21 @@
+//1. write a program to add two number and check sum is equal to the first number or not
+
+
+public class Checksum {
+	 public static void main(String args[])
+	 {
+		 int num1 =10;
+		 int num2 = 20;
+		 int sum=;
+		 System.out.println("the sum of"+ sum);
+		 if (sum == num1) {
+			 System.out.println("the sum of num1 ans num2 is equals to num1 one");
+		 }
+			 else {
+				 System.out.println("the sum is not equal to num1");
+			 
+		 }
+	 }
+}
+		 
+		 
